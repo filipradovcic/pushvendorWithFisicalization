32f9b6836c291fa31f411820fa08772cf93ba252
filip <filip.radovcic@gmail.com>
2016-08-25 21:25:37 +0200

    made cases what to do depending on which payment type, need to make dummy function for ipg and cash

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,9 +6,20 @@ class PaymentsController < ApplicationController
                    sale_id: params[:payments][:sale_id])
 
 
+    if(params[:payments][:payment_type]== 'credit_card')
+       logger.info('redirect user to internet payment gateway')
+    elsif(params[:payments][:payment_type]== 'cash')
+       logger.info('fisiclize invoice')
+    else
+      logger.info('wait for the back office to clear the payment')
+    end
+
+
     fiscalizer = Fiscalizer.new
 
-    logger.info('making')
+    logger.info('Fizicaling invoice')
+
+
     respond_to do |format|
       format.js
     end
